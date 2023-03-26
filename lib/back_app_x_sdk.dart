4b54fa4578232ed6712b2c61755b36f874880d8a
KvRae <karamelmannai@gmail.com>
2023-03-26 03:05:54 +0100

    working with login exemple

--- lib/back_app_x_sdk.dart
+++ lib/back_app_x_sdk.dart
@@ -1,16 +1,34 @@
 library back_app_x_sdk;
+import 'package:http/http.dart' as http;
+
+
+//here you can add your own variables
+
+
+class BackAppX {
+  static Auth auth = Auth();
+}
 
 
 class Auth {
-  void login(username, password) {
+  static const String _baseUrl = 'http://localhost:8000/api';
+  static const _loginUrl = '$_baseUrl/login';
+  static const String _registerUrl = '$_baseUrl/register';
+  static void login(username, password) {
     print('login');
-  }
-  void logout() {
-    print('logout');
+    http.post(Uri.parse(_loginUrl), body: {
+      'username': username,
+      'password': password,
+    }).then((response) {
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    });
+    //post request to server
   }
 
-  void register(name, email, password) {
+  static void register(name, email, password) {
     print('register');
+    //post request to server
   }
 
 
